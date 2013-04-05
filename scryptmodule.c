967f2ef35b17e3630bc42ba3fc1313b70ee446ab
Tydus <Tydus@Tydus.org>
2013-04-05 16:33:03 +0900

    Add compatibility of python3

--- scryptmodule.c
+++ scryptmodule.c
@@ -6,15 +6,27 @@ static PyObject *scrypt_getpowhash(PyObject *self, PyObject *args)
 {
     char *output;
     PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
     PyStringObject *input;
+#endif
     if (!PyArg_ParseTuple(args, "S", &input))
         return NULL;
     Py_INCREF(input);
     output = PyMem_Malloc(32);
 
+#if PY_MAJOR_VERSION >= 3
+    scrypt_1024_1_1_256((char *)PyBytes_AsString((PyObject*) input), output);
+#else
     scrypt_1024_1_1_256((char *)PyString_AsString((PyObject*) input), output);
+#endif
     Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
     value = Py_BuildValue("s#", output, 32);
+#endif
     PyMem_Free(output);
     return value;
 }
@@ -24,6 +36,22 @@ static PyMethodDef ScryptMethods[] = {
     { NULL, NULL, 0, NULL }
 };
 
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef ScryptModule = {
+    PyModuleDef_HEAD_INIT,
+    "ltc_scrypt",
+    "...",
+    -1,
+    ScryptMethods
+};
+
+PyMODINIT_FUNC PyInit_ltc_scrypt(void) {
+    return PyModule_Create(&ScryptModule);
+}
+
+#else
+
 PyMODINIT_FUNC initltc_scrypt(void) {
     (void) Py_InitModule("ltc_scrypt", ScryptMethods);
 }
+#endif
